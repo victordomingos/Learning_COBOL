fea52cbc587a0111743dbc2f94dd375fd1c677c8
Victor Domingos <editor.arcosonline@gmail.com>
2019-09-13 11:53:00 +0100

    11_clear_screen.cob

---
+++ 11_clear_screen.cob
@@ -0,0 +1,20 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+
+
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY SPACE ERASE EOS
+            DISPLAY "Hello world" AT 1020.
+            STOP " ".
+       END PROGRAM YOUR-PROGRAM-NAME.
