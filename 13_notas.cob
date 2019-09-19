7f6c369a93bc493dd8455c7faa01c5d89c4860bd
Victor Domingos <editor.arcosonline@gmail.com>
2019-09-19 15:17:12 +0100

    13_notas.cob

--- 13_notas.cob
+++ 13_notas.cob
@@ -22,12 +22,13 @@
       *-----------------------------------------------------------------
        WORKING-STORAGE SECTION.
        77  NOME    PIC A(20)   VALUE SPACES.
-       77  NOTA1   PIC 99      VALUE ZERO.
-       77  NOTA2   PIC 99      VALUE ZERO.
-       77  NOTA3   PIC 99      VALUE ZERO.
-       77  NOTA4   PIC 99      VALUE ZERO.
+       77  NOTA1   PIC S99      VALUE -1.
+       77  NOTA2   PIC S99      VALUE -1.
+       77  NOTA3   PIC S99      VALUE -1.
+       77  NOTA4   PIC S99      VALUE -1.
+
        77  MEDIA   PIC 99V99   VALUE ZERO.
-       77  MEDIA-M PIC Z9.9    VALUE ZERO.
+       77  NOTA-M  PIC Z9.9    VALUE ZERO.
 
       *=================================================================
        PROCEDURE DIVISION.
@@ -46,22 +47,22 @@
                ACCEPT NOME
            END-PERFORM.
 
-           PERFORM UNTIL (NOTA1 > 0) AND (NOTA1 <= 20)
+           PERFORM UNTIL (NOTA1 >= 0) AND (NOTA1 <= 20)
                DISPLAY "Por favor, introduza a nota 1:"
                ACCEPT NOTA1
            END-PERFORM.
 
-           PERFORM UNTIL (NOTA2 > 0) AND (NOTA2 <= 20)
+           PERFORM UNTIL (NOTA2 >= 0) AND (NOTA2 <= 20)
                DISPLAY "Por favor, introduza a nota 2:"
                ACCEPT NOTA2
            END-PERFORM.
 
-           PERFORM UNTIL (NOTA3 > 0) AND (NOTA3 <= 20)
+           PERFORM UNTIL (NOTA3 >= 0) AND (NOTA3 <= 20)
                DISPLAY "Por favor, introduza a nota 3:"
                ACCEPT NOTA3
            END-PERFORM.
 
-           PERFORM UNTIL (NOTA4 > 0) AND (NOTA4 <= 20)
+           PERFORM UNTIL (NOTA4 >= 0) AND (NOTA4 <= 20)
                DISPLAY "Por favor, introduza a nota 4:"
                ACCEPT NOTA4
            END-PERFORM.
@@ -69,18 +70,26 @@
 
        CALC-AVG-PROCEDURE.
            COMPUTE MEDIA = (NOTA1+NOTA2+NOTA3+NOTA4)/4.
-           MOVE MEDIA TO MEDIA-M.
 
 
 
        REPORT-PROCEDURE.
            DISPLAY " "
-           DISPLAY "   N1: " NOTA1.
-           DISPLAY "   N2: " NOTA2.
-           DISPLAY "   N3: " NOTA3.
-           DISPLAY "   N4: " NOTA4.
+           MOVE NOTA1 TO NOTA-M
+           DISPLAY "   N1: " NOTA-M.
+
+           MOVE NOTA2 TO NOTA-M
+           DISPLAY "   N2: " NOTA-M.
+
+           MOVE NOTA3 TO NOTA-M
+           DISPLAY "   N3: " NOTA-M.
+
+           MOVE NOTA4 TO NOTA-M
+           DISPLAY "   N4: " NOTA-M.
+
            DISPLAY " "
-           DISPLAY "Media: " MEDIA-M " valores.".
+           MOVE MEDIA TO NOTA-M.
+           DISPLAY "Media: " NOTA-M " valores.".
            IF MEDIA < 10
                DISPLAY "REPROVADO"
            ELSE
